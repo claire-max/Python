149f33e8ea9faf99f83ed0a2561be2cfe50d177b
ClaireFree <claire@robingoodfoods.com>
2023-03-31 17:39:17 -0700

    finished random animal quiz

--- QuizGame.py
+++ QuizGame.py
@@ -1,14 +1,45 @@
-print("Welcome to my quiz!")
+print("Welcome to my Random animal quiz!")
 
 playing = input("Do you want to play? ")
+score =0
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play:) ")
 
-answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+answer = input("Which animal is the biggest? ")
+if answer.lower() == "blue whale":
     print('Correct!')
+    score += 1
 else: 
     print("Incorrect!")
+
+
+answer = input("What do snakes use to smell? ")
+if answer.lower() == "their tongue":
+    print('Correct!')
+    score += 1
+else: 
+    print("Incorrect!")
+
+
+answer = input("What animal has a purple tongue? ")
+if answer.lower() == "giraffe":
+    print('Correct!')
+    score += 1
+else: 
+    print("Incorrect!")
+
+
+answer = input("How many hearts does an octopus have? ")
+if answer.lower() == "3":
+    print('Correct!')
+    score += 1
+else: 
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+
+print("You got " + str((score / 4) * 100) + "%")
+
